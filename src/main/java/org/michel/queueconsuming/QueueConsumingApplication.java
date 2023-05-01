83d8da17bf0b3c35ab440a767ecca73cc2ac315e
Michel Henrique Alves <michel.alves@sensedia.com>
2023-05-01 14:31:32 -0300

    criacao, envio e leitura de mensagens no sqs

---
+++ src/main/java/org/michel/queueconsuming/QueueConsumingApplication.java
@@ -0,0 +1,13 @@
+package com.sensedia.queueconsuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueConsumingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueConsumingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sensedia/queueconsuming/QueueConsumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensedia.queueconsuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueConsumingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
